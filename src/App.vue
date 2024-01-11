387f90c7711f6b940429107712310fdcbdd16ef8
5621348 <antonclassic1@gmail.com>
2024-01-11 04:03:26 +0300

    V1.0.2 Clining: helloWorld files

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!--HelloWorld msg="Welcome to Your Vue.js App"/-->
     <SimonTheGame />
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
 import SimonTheGame from "@/components/SimonTheGame";
 
 export default {
   name: 'App',
   components: {
-    SimonTheGame,
-    //HelloWorld
+    SimonTheGame
   }
 }
 </script>
